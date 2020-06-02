0a736705944a326905f2ebd24f45dcaaac9300bf
adario <24436551+adario7@users.noreply.github.com>
2020-06-02 19:24:46 +0200

    bugfix on map read

--- cpp/types.cc
+++ cpp/types.cc
@@ -178,8 +178,8 @@ void r_map(const string& fname, const NType& t, ostream& o) {
 	const NType& k_t = *list[0], &v_t = *list[1]; // checks already performed when writing
 	o << "\t\t\tsize_t __" << fname << "_sz; ; __s >> __" << fname << "_sz;" << endl
 		<< "\t\t" << _GENERATE_FOR
-		<< "\t\t\t" << k_t << " __k_" << fname << ";" << endl
-		<< "\t\t\t" << v_t << " __v_" << fname << ";" << endl;
+		<< "\t\t\t" << to_cpp_type(k_t) << " __k_" << fname << ";" << endl
+		<< "\t\t\t" << to_cpp_type(v_t) << " __v_" << fname << ";" << endl;
 	deserialize_value("__k_" + fname, k_t, o);
 	deserialize_value("__v_" + fname, v_t, o);
 	o << "\t\t\t" << fname << "[__k_" << fname << "] = __v_" << fname << ";" << endl
